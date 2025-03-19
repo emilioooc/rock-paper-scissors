14e0dc7da3d33258b47735fa8e41d7d37c67d628
emilioooc <164695697+emilioooc@users.noreply.github.com>
2025-03-19 18:31:59 -0300

    Creacion de la funcion de eleccion el jugador

--- script.js
+++ script.js
@@ -18,4 +18,22 @@ function getChoice (number){
     };
 };
 
+
+let option = prompt ('1-Tijera \n 2-Papel \n 3-Piedra');
+
+function getHumanChoice(option){
+    let humanChoice = null;
+    if (option == 1){
+        humanChoice = "tijera";
+        return humanChoice;
+    } else if (option == 2){
+        humanChoice = "papel";
+        return humanChoice;
+    } else {
+        humanChoice = "piedra";
+        return humanChoice;
+    }
+};
+
+console.log(getHumanChoice(option));
 console.log(getComputerChoice());
