70e4f0f721162cc3f5d14e783eedc74a01509549
emilioooc <164695697+emilioooc@users.noreply.github.com>
2025-03-23 19:53:18 -0300

    Piedra-Papel-Tijera TERMINADO (reformado)

--- script.js
+++ script.js
@@ -1,68 +1,73 @@
 
-function getComputerChoice(){
-    let number = Math.floor(Math.random() * 3);
-    return getChoice(number)
-};
+function playGame(){
+    let computerScore = 0;
+    let humanScore = 0;
+    let empates = 0;
 
-let computerChoice = null;
-function getChoice (number){
+    let resultados = playRound(computerScore, humanScore, empates);
+    
+    computerScore = resultados.cScore;
+    humanScore = resultados.hScore;
+    empates = resultados.empate;
 
-    if (number ==1){
-        computerChoice = "tijera"
-        return computerChoice;
-    } else if( number ==2){
-        computerChoice = "piedra"
-        return computerChoice;
-    } else{
-        computerChoice = "papel"
-        return computerChoice;
-    };
-};
+    console.log("Computer Score Global: ", computerScore);
+    console.log("Human Score Global; ", humanScore);
+    console.log("Empates Totales: ", empates);
 
+    if (computerScore > humanScore){
+        console.log("Ganador PC")
+    }else if ( computerScore < humanScore){
+        console.log("Ganador Human")
+    } else {console.log("Empate")}
+}
 
-let option = prompt ('1-Tijera \n 2-Papel \n 3-Piedra');
-let humanChoice = null;
+function playRound (cScore, hScore, empate){
+    for (let i = 0 ; i < 5 ; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
 
-function getHumanChoice(option){
+        if (humanChoice == "tijera" && computerChoice == "papel"){
+            hScore = hScore +1;
+        } else if (humanChoice == "tijera" && computerChoice == "piedra"){
+            cScore = cScore +1;
+        } else if (humanChoice == "papel" && computerChoice == "piedra"){
+            hScore = hScore +1;
+        }else if (humanChoice == "papel" && computerChoice == "tijera"){
+            cScore = cScore +1;
+        }else if (humanChoice == "piedra" && computerChoice == "tijera"){
+            hScore = hScore +1;
+        }else if (humanChoice == "piedra" && computerChoice == "papel"){
+            cScore = cScore +1;
+        }else {empate = empate +1}
+
+        console.log("Ronda: ", i+1);
+        console.log("Human eleccion: ", humanChoice);
+        console.log("PC eleccion: ", computerChoice);
+        console.log("Homan Score Round: ",hScore);
+        console.log("Computer Score Round: ", cScore);
+        console.log("Empates: ", empate)
+    }
+    return { cScore, hScore, empate };
+}
+
+function getHumanChoice (){
+    let option = prompt ("1-Tijera \n 2-Papel \n 3-Piedra");
     if (option == 1){
-        humanChoice = "tijera";
-        return humanChoice;
-    } else if (option == 2){
-        humanChoice = "papel";
-        return humanChoice;
-    } else {
-        humanChoice = "piedra";
-        return humanChoice;
-    }
-};
+        option = "tijera"
+    }else if (option == 2){
+        option = "papel"
+    }else {option = "piedra"}
+    return option;
+}
 
-let humanScore = 0;
-let computerScore = 0;
+function getComputerChoice(){
+    let option = Math.floor(Math.random() * 3);
+    if (option == 1){
+        option = "tijera"
+    }else if (option == 2){
+        option = "papel"
+    }else {option = "piedra"}
+    return option;
+}
 
-function playRound(){
-    getHumanChoice(option);
-    getComputerChoice();
-    if (humanChoice == "tijera" && computerChoice == "papel"){
-        humanScore = humanScore + 1;
-        return humanScore;
-    } else if (humanChoice == "tijera" && computerChoice == "piedra"){
-        computerScore = computerScore + 1;
-        return computerScore;
-    } else if (humanChoice == "papel" && computerChoice == "tijera"){
-        computerScore = computerScore + 1;
-        return computerScore;
-    } else if (humanChoice == "papel" && computerChoice == "piedra"){
-        humanScore = humanScore + 1;
-        return humanScore;
-    } else if (humanChoice == "piedra" && computerChoice == "papel"){
-        computerScore = computerScore + 1;
-        return computerScore;
-    } else if (humanChoice == "piedra" && computerChoice == "tijera"){
-        humanScore = humanScore + 1;
-        return humanScore;
-    } else {
-        return "empate"
-    }
-};
-
-console.log (playRound())
+console.log(playGame());
